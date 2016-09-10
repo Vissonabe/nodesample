92c742e4d58e26af49766d94dde2aa0fdfade608
Vissonabe <vissonabe@gmail.com>
2016-09-10 16:14:56 +0530

    commented

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var connect = require('connect');
 var app      = express(); 
 var port     = process.env.PORT || 8080;  
 // Configuration 
-app.use(express.static(__dirname + '/public')); 
-app.use(connect.logger('dev')); 
-app.use(connect.json()); 
-app.use(connect.urlencoded());  
+//app.use(express.static(__dirname + '/public')); 
+//app.use(connect.logger('dev')); 
+//app.use(connect.json()); 
+//app.use(connect.urlencoded());  
 // Routes  
 
 //require('./routes/routes.js')(app);  
