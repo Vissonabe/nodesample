11233ef71d39b0e2947586e08652a51848c26752
Vissonabe <vissonabe@gmail.com>
2016-09-10 16:38:52 +0530

    using routes

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var connect = require('connect');
 var app      = express(); 
 var port     = process.env.PORT || 8080;  
 // Configuration 
-//app.use(express.static(__dirname + '/public')); 
-//app.use(connect.logger('dev')); 
-//app.use(connect.json()); 
-//app.use(connect.urlencoded());  
+app.use(express.static(__dirname + '/public')); 
+app.use(connect.logger('dev')); 
+app.use(connect.json()); 
+app.use(connect.urlencoded());  
 // Routes  
 
-//require('./routes/routes.js')(app);  
+require('./routes/routes.js')(app);  
 
-app.set('views', __dirname + '/views');
-  app.engine('html', require('ejs').renderFile);
-  app.get('/', function(req, res) {
-    res.render('index.html');
-  });
+//app.set('views', __dirname + '/views');
+//  app.engine('html', require('ejs').renderFile);
+//  app.get('/', function(req, res) {
+//    res.render('index.html');
+//  });
 
 app.listen(port);  
 
