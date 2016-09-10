1e34503c0a55e6b67f42d929409f6b6420077332
Vissonabe <vissonabe@gmail.com>
2016-09-10 16:12:07 +0530

    adding views to app

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-var app = function () {
-  var http = require('http');
-  var express = require('express');
-  var app = express();
+var express  = require('express'); 
+var connect = require('connect'); 
+var app      = express(); 
+var port     = process.env.PORT || 8080;  
+// Configuration 
+app.use(express.static(__dirname + '/public')); 
+app.use(connect.logger('dev')); 
+app.use(connect.json()); 
+app.use(connect.urlencoded());  
+// Routes  
 
-  app.set('views', __dirname + '/views');
+//require('./routes/routes.js')(app);  
+
+app.set('views', __dirname + '/views');
   app.engine('html', require('ejs').renderFile);
   app.get('/', function(req, res) {
     res.render('index.html');
   });
 
-  return app;
-}();
+app.listen(port);  
 
-module.exports = app;
+console.log('The App runs on port ' + port);
